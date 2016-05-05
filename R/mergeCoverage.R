9b2dc44fba0598ad78298d64925750a8b907fffc
Jianhong Ou <jianhong.ou@umassmed.edu>
2016-05-05 14:55:42 +0000

    fix the error when not all the seqnames is in bedgraphs for CPsites.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_3/madman/Rpacks/InPAS@117149 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/mergeCoverage.R
+++ R/mergeCoverage.R
@@ -21,30 +21,51 @@ mergeCoverage <- function(coverage, groupList, genome, BPPARAM=NULL){
         
         for(i in 1:length(y)){
             if(!is.null(BPPARAM)){
-                cv <- bplapply(covFiles[y[[i]]], function(.ele){
+                cv <- bptry(bplapply(covFiles[y[[i]]], function(.ele){
                     cvg <- NULL
                     load(.ele)
                     cvg <- cvg[seqnames]
+                    names(cvg) <- seqnames
                     idx <- sapply(cvg, is.null)
                     cvg[idx] <- seqRle[idx]
                     cvg
-                }, BPPARAM=BPPARAM)
+                }, BPPARAM=BPPARAM))
+                while(!all(bpok(cv))){
+                    cv <- bptry(bplapply(covFiles[y[[i]]], function(.ele){
+                        cvg <- NULL
+                        load(.ele)
+                        cvg <- cvg[seqnames]
+                        names(cvg) <- seqnames
+                        idx <- sapply(cvg, is.null)
+                        cvg[idx] <- seqRle[idx]
+                        cvg
+                    }, BPREDO=cv, BPPARAM=BPPARAM))
+                }
                 if(length(cv)<10){
                     for(m in (length(cv)+1):10){
                         cv[[m]] <- seqRle
                     }
                 }
-                cov[[j]] <- bplapply(seqnames, function(s){
+                cov[[j]] <- bptry(bplapply(seqnames, function(s){
                     cv[[1]][[s]] + cv[[2]][[s]] + cv[[3]][[s]] +
                         cv[[4]][[s]] + cv[[5]][[s]] + cv[[6]][[s]] +
                         cv[[7]][[s]] + cv[[8]][[s]] + cv[[9]][[s]] +
                         cv[[10]][[s]] + cov[[j]][[s]]
-                }, BPPARAM=BPPARAM)
+                }, BPPARAM=BPPARAM))
+                while(!all(bpok(cov[[j]]))){
+                    cov[[j]] <- bptry(bplapply(seqnames, function(s){
+                        cv[[1]][[s]] + cv[[2]][[s]] + cv[[3]][[s]] +
+                            cv[[4]][[s]] + cv[[5]][[s]] + cv[[6]][[s]] +
+                            cv[[7]][[s]] + cv[[8]][[s]] + cv[[9]][[s]] +
+                            cv[[10]][[s]] + cov[[j]][[s]]
+                    }, BPREDO=cov[[j]], BPPARAM=BPPARAM))
+                }
             }else{
                 cv <- lapply(covFiles[y[[i]]], function(.ele){
                     cvg <- NULL
                     load(.ele)
                     cvg <- cvg[seqnames]
+                    names(cvg) <- seqnames
                     idx <- sapply(cvg, is.null)
                     cvg[idx] <- seqRle[idx]
                     cvg
